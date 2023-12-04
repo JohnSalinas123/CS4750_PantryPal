8db67e32839dc27390db13e13e7edabd3821f030
An Le <lkbaoan@gmail.com>
2023-12-03 20:54:17 -0800

    Add ability to display bmi resutl

--- app/src/main/java/com/example/pantry_pal/ui/bmi_calculator/BmiCalculatorFragment.kt
+++ app/src/main/java/com/example/pantry_pal/ui/bmi_calculator/BmiCalculatorFragment.kt
@@ -8,6 +8,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.pantry_pal.databinding.FragmentBmiCalculatorBinding
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import kotlin.math.pow
 
 class BmiCalculatorFragment : Fragment() {
 
@@ -28,10 +31,10 @@ class BmiCalculatorFragment : Fragment() {
         _binding = FragmentBmiCalculatorBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        val textView: TextView = binding.mealPlan
-//        mealPlanViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
+        binding.calculate.setOnClickListener{
+            val result = calculateBmi()
+
+        }
         return root
     }
 
@@ -40,7 +43,33 @@ class BmiCalculatorFragment : Fragment() {
         _binding = null
     }
 
-    fun calculateBmi() {
+    private fun calculateBmi(){
+        val convert = 703
 
+        val h1 = binding.height1.text.toString().toDoubleOrNull()
+        val h2 = binding.height2.text.toString().toDoubleOrNull()
+        val w =binding.weight.text.toString().toDoubleOrNull()
+
+        if (h1 == null || h2 == null || w == null || h1 == 0.0 || h2 == 0.0) {
+            displayBmi(0.0)
+            return
+        } else {
+            val bmiResult: Double = if (unitSelected() == 1) {
+                w/((h1+(h2*0.01)).pow(2))
+            } else {
+                convert * w/((h1*12+h2).pow(2))
+            }
+            displayBmi(bmiResult)
+        }
+    }
+
+    private fun unitSelected() : Int {
+        when (binding.unitOption.checkedRadioButtonId) {
+            binding.metricUnit.id -> return 1
+        }
+        return 0
+    }
+    private fun displayBmi(bmi: Double) {
+        binding.displayBmi.text = String.format("BMI: %.2f", bmi)
     }
 }
