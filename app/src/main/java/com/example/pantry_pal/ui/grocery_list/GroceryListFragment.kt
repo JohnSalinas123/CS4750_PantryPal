b4e6f2970be039fb1941f74380ed06b4f8c66abe
Brian Nguyen <nguyenbrian726@gmail.com>
2023-12-04 18:07:07 -0800

    added grocery list stuff

--- app/src/main/java/com/example/pantry_pal/ui/grocery_list/GroceryListFragment.kt
+++ app/src/main/java/com/example/pantry_pal/ui/grocery_list/GroceryListFragment.kt
@@ -4,34 +4,59 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.pantry_pal.R
 import com.example.pantry_pal.databinding.FragmentGroceryListBinding
 
 class GroceryListFragment : Fragment() {
 
     private var _binding: FragmentGroceryListBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var viewModel: GroceryListViewModel
+    private lateinit var adapter: ArrayAdapter<String>
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val groceryListViewModel =
-            ViewModelProvider(this).get(GroceryListViewModel::class.java)
-
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        viewModel = ViewModelProvider(this).get(GroceryListViewModel::class.java)
         _binding = FragmentGroceryListBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGroceryList
-        groceryListViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        adapter = object : ArrayAdapter<String>(requireContext(), R.layout.list_item_grocery, R.id.textViewItem, mutableListOf()) {
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                val view = super.getView(position, convertView, parent)
+                val item = getItem(position)
+                val textViewItem = view.findViewById<TextView>(R.id.textViewItem)
+                val buttonDeleteItem = view.findViewById<Button>(R.id.buttonDeleteItem)
+
+                textViewItem.text = item
+                buttonDeleteItem.setOnClickListener {
+                    item?.let { it1 -> viewModel.removeItem(it1) }
+                }
+
+                return view
+            }
         }
+
+        binding.listViewGrocery.adapter = adapter
+
+        viewModel.groceryItems.observe(viewLifecycleOwner) { items ->
+            adapter.clear()
+            adapter.addAll(items)
+            adapter.notifyDataSetChanged()
+        }
+
+        binding.buttonAddItem.setOnClickListener {
+            val newItem = binding.editTextItem.text.toString()
+            if (newItem.isNotEmpty()) {
+                viewModel.addItem(newItem)
+                binding.editTextItem.text.clear()
+            }
+        }
+
         return root
     }
 
--- app/src/main/java/com/example/pantry_pal/ui/grocery_list/GroceryListViewModel.kt
+++ app/src/main/java/com/example/pantry_pal/ui/grocery_list/GroceryListViewModel.kt
@@ -4,10 +4,23 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+
 class GroceryListViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is grocery list Fragment"
+    private val _groceryItems = MutableLiveData<MutableList<String>>(mutableListOf())
+    val groceryItems: LiveData<MutableList<String>> = _groceryItems
+
+    // Function to add an item
+    fun addItem(item: String) {
+        val updatedList = groceryItems.value ?: mutableListOf()
+        updatedList.add(item)
+        _groceryItems.value = updatedList
     }
-    val text: LiveData<String> = _text
+
+    // Function to remove an item
+    fun removeItem(item: String) {
+        val updatedList = groceryItems.value ?: mutableListOf()
+        updatedList.remove(item)
+        _groceryItems.value = updatedList
+    }
 }
